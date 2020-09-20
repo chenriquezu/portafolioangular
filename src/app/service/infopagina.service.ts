6081dc75132937f9a0acb099e9e18c15b1a84f81
cristian <cristian.henriquez.urbina6@gmail.com>
2020-09-19 20:57:14 -0400

    termino seccion 5

--- src/app/service/infopagina.service.ts
+++ src/app/service/infopagina.service.ts
@@ -9,16 +9,30 @@ import { infopagina } from '../interfaces/info-pagina.interface';
 export class InfopaginaService {
 
 info: infopagina = {};
+equipo: any = [];
 cargada: false;
 
 
   constructor(private http: HttpClient) 
   { 
-     this.http.get('assets/data/data-pagina.json')
-              .subscribe ( (resp: infopagina) => {
-                this.info = resp;
-                console.log(resp);
-     });
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private  cargarInfo(){
+    this.http.get('assets/data/data-pagina.json')
+        .subscribe ( (resp: infopagina) => {
+    this.info = resp;
+    });
+  }
+
+  private cargarEquipo(){
+     this.http.get('https://angular-html-6c8db.firebaseio.com/equipo.json')
+         .subscribe((resp: any[])  => {
+           this.equipo = resp;
+           console.log(resp);
+         })
+
 
   }
 }
